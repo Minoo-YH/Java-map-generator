810e4e637dea34bc38ce88e1248f30b6aa05ccaa
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-08-25 08:41:14 +0300

    add road tile

---
+++ RoadTile.java
@@ -0,0 +1,12 @@
+public class RoadTile implements Tile {
+
+    @Override
+    public char getCharacter() {
+        return 'R';
+    }
+
+    @Override
+    public String getType() {
+        return "road";
+    }
+}
