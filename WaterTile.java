112924dfb8b4765556792178930662868b24fd79
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-08-25 08:42:33 +0300

    add water tile

---
+++ WaterTile.java
@@ -0,0 +1,12 @@
+public class WaterTile implements Tile {
+
+    @Override
+    public char getCharacter() {
+        return 'W';
+    }
+
+    @Override
+    public String getType() {
+        return "water";
+    }
+}
