d85ee84571ce7783a66c50a83914754678e9a499
Minoo Yaghoubi <155952014+Minoo-YH@users.noreply.github.com>
2026-08-25 08:41:27 +0300

    add building tile

---
+++ BuildingTile.java
@@ -0,0 +1,12 @@
+public class BuildingTile implements Tile {
+
+    @Override
+    public char getCharacter() {
+        return 'B';
+    }
+
+    @Override
+    public String getType() {
+        return "building";
+    }
+}
